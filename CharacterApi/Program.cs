dfdfa9b183eb30a394fce506551ee807e8e71abb
igor.ribeiro <igorgui3103@gmail.com>
2026-02-18 12:24:15 -0300

    correção scalar

--- CharacterApi/Program.cs
+++ CharacterApi/Program.cs
@@ -19,11 +19,10 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
-}
+
+app.MapOpenApi();
+app.MapScalarApiReference();
+
 
 app.UseHttpsRedirection();
 
